63aed4cf32b25a82cd15894d5bd43ff342499db1
MliKiowa <nineto0@163.com>
2022-10-08 21:22:41 +0800

    Update Test.py

--- tool/Test.py
+++ tool/Test.py
@@ -1,27 +1,13 @@
 # -*- coding: utf-8 -*-
 import requests
 import sys,json,os
-def take_middle_text(txt,txt_s,txt_e='',seeks=0,seeke=0):#取中间文本函数
-    try:
-        if txt_e or seeks or seeke:
-            pass
-        else:
-            raise 1
-        s_1 = txt.find(txt_s)
-        if s_1 == -1:
-            raise 1
-        l_1 = len(txt_s)
-        if txt_e:
-            s_2 = txt.find(txt_e,s_1)
-            if s_1 == -1 or s_2 == -1:
-                return False
-            return txt[s_1+l_1:s_2]
-        if seeks:
-            return txt[s_1-seeks:s_1]
-        if seeke:
-            return txt[s_1+l_1:s_1+l_1+seeke]
-    except:
-        return '传参错误或未找到传参文本'
+def getmidstring(html, start_str, end):
+    start = html.find(start_str)
+    if start >= 0:
+        start += len(start_str)
+        end = html.find(end, start)
+        if end >= 0:
+            return html[start:end].strip()
 
 url = "https://api.github.com/repos/Mlikiowa/BlogFriend/issues" 
 res = requests.get(url).json()
@@ -29,7 +15,7 @@ print(res)
 try:
   for key in range(len(res)):
     print(res[key]['body'])
-    site = take_middle_text(res[key]['body'],"\`\`\`","\`\`\`")
+    site = getmidstring(res[key]['body'],"\`\`\`","\`\`\`")
     print(site)
     tempsite = json.loads(site)
     os.system(f"lighthouse {tempsite.url} --output json --output-path ./{tempsite.url} .json")
