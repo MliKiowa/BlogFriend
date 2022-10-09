1ec47b1da8757fc7852e545ef4f10a498da77d9f
MliKiowa <nineto0@163.com>
2022-10-09 14:29:21 +0800

    Update Generate.py

--- tool/Generate.py
+++ tool/Generate.py
@@ -20,6 +20,7 @@ for key in range(len(res)):
     sitejson = "{" + getmidstring(res[key]['body'],"{","}") + "}"    
     if (len(res[key]["labels"]) >= 0) and (res[key]["labels"][0]["name"] == "pass"):      
       tsite.update(json.loads(sitejson))
-sitey["items"] = sitey["items"].update(tsite)
-stream = open("test/friend.yml", 'w+')
-yaml.safe_dump(sitey, stream, default_flow_style=False,allow_unicode=True)
+print(tsite)
+# sitey["items"] = sitey["items"].update(tsite)
+# stream = open("test/friend.yml", 'w+')
+# yaml.safe_dump(sitey, stream, default_flow_style=False,allow_unicode=True)
