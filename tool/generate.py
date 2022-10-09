8a781469d7b4f0e7360c4c9264f383f95a9d99d9
MliKiowa <nineto0@163.com>
2022-10-09 13:24:14 +0800

    Create generate.py

---
+++ tool/generate.py
@@ -0,0 +1,20 @@
+# -*- coding: utf-8 -*-
+import requests
+import sys,json,os,yaml
+def getmidstring(html, start_str, end):
+    start = html.find(start_str)
+    if start >= 0:
+        start += len(start_str)
+        end = html.find(end, start)
+        if end >= 0:
+            return html[start:end].strip()
+
+url = "https://api.github.com/repos/Mlikiowa/BlogFriend/issues" 
+res = requests.get(url).json()
+for key in range(len(res)):
+    id = res[key]['number']
+    sitejson = "{" + getmidstring(res[key]['body'],"{","}") + "}"    
+    if (len(res[key]["labels"]) >= 0) and (res[key]["labels"]["name"] == "pass"):
+      sitey = yaml.load(sitejson)
+      stream = open("test/friend.yaml", 'w+')
+      yaml.safe_dump(sitey, stream, default_flow_style=False)
