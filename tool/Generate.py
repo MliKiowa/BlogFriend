3c9907e3d811ea97f83b7df70fa712fecb12e257
MliKiowa <nineto0@163.com>
2022-10-09 14:07:41 +0800

    Update Generate.py

--- tool/Generate.py
+++ tool/Generate.py
@@ -11,11 +11,16 @@ def getmidstring(html, start_str, end):
 
 url = "https://api.github.com/repos/Mlikiowa/BlogFriend/issues" 
 res = requests.get(url).json()
+
+f = open('friend.yml', 'r', encoding='utf-8')
+sitey = yaml.load(f.read(), Loader=yaml.FullLoader)
+print(result, type(result))
 for key in range(len(res)):
+    tsite = {}
     id = res[key]['number']
     sitejson = "{" + getmidstring(res[key]['body'],"{","}") + "}"    
-    if (len(res[key]["labels"]) >= 0) and (res[key]["labels"][0]["name"] == "pass"):
-      print(sitejson)  
-      sitey = yaml.load(sitejson, Loader=yaml.FullLoader)
-      stream = open("test/friend.yaml", 'w+')
-      yaml.safe_dump(sitey, stream, default_flow_style=False,allow_unicode=True)
+    if (len(res[key]["labels"]) >= 0) and (res[key]["labels"][0]["name"] == "pass"):      
+      tsite.append(json.loads(sitejson))
+sitey["items"].append(tsite)
+stream = open("test/friend.yaml", 'w+')
+yaml.safe_dump(sitey, stream, default_flow_style=False,allow_unicode=True)
